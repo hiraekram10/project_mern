410389e15aa2573334bad8c150d35bf8d3e777c5
hiraekram10 <heraiqram123@gmail.com>
2025-02-10 14:05:07 +0500

    bakend removed

--- backend-app/costant/enum.js
+++
@@ -1,7 +0,0 @@
-export const enums = {
-   Error_connection: "somthing went wrong",
-   No_Users_Found:"users not found",
-   Succes_Mssg:"Added succesfully",
-   Deleted_Mssg:"Deleted succesfully,",
-   Updated_Mssg:"updated succesfully,",
-} 
